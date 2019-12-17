1037bfd22e5450410e1cdb224b19b4d6239fa788
Alessio Gambi <alessio.gambi@uni-passau.de>
2019-12-17 19:47:54 +0100

    Restructure project files to have a common test script and set of assertions per task. Keeping assertions separated by task is a design decision.
    Closes issue #3

--- task-1/test-task-1.sh
+++ task-1/test-task-1.sh
@@ -10,34 +10,53 @@ fi
 TEST_HOME="$(realpath "$1")"
 PROJECT_HOME="$(realpath "$2")"
 
-# Ensures that the secret.code file is where it is supposed to be
-cp ${TEST_HOME}/secret.code ${PROJECT_HOME}
+# The following paths are hardcoded because the structure of the repositories is FIXED
+TASK_HOME="$(realpath "${PROJECT_HOME}/task-1")"
+ASSERTIONS_HOME="$(realpath "${TEST_HOME}/../../assertions")"
+
+# SWAP the secret.code if there's one already (manual test)
+if [ -e ${TASK_HOME}/secret.code ]; then
+    mv ${TASK_HOME}/secret.code ${TASK_HOME}/secret.code.orig
+fi
+
+# Ensures that the secret.code defined inside the test is where it is supposed to be
+cp ${TEST_HOME}/secret.code ${TASK_HOME}
 
 # Move in program folder
-cd ${PROJECT_HOME}
+cd ${TASK_HOME}
 
 # Pipe the inputs into the program, collect both output and error
 cat ${TEST_HOME}/input | \
         node master-mild.js --enable-aspects > ${TEST_HOME}/output 2> ${TEST_HOME}/error
 
+# Not really needed
+cd -
+
 # Clean up the project folder
-rm ${PROJECT_HOME}/secret.code
+rm ${TASK_HOME}/secret.code
+
+# Restore the files that were there before execution
+if [ -e ${TASK_HOME}/secret.code.orig ]; then
+    mv ${TASK_HOME}/secret.code.orig ${TASK_HOME}/secret.code
+fi
+
+###########
 
 # Move in test folder
 cd ${TEST_HOME}
 
-# Clean up stale report file. This should never happen...
+# Clean up stale report file.
 if [ -e test-report ]; then rm test-report; fi
 
 # Check common assertions game-assertions
-for A in $(find ${TEST_HOME}/game-assertions/ -type f); do
+for A in $(find ${ASSERTIONS_HOME}/game-assertions/ -type f); do
     # Assumption is that assertion produces error messages in case they fail
     $A 2> >(sed 's/^/    /') | cat >> test-report
 done
 
 # If the test leads to a WON game, check won game assertions
 if [ "$(cat ${TEST_HOME}/game.result)" == "won" ]; then
-    for A in $(find ${TEST_HOME}/won-game-assertions/ -type f); do
+    for A in $(find ${ASSERTIONS_HOME}/won-game-assertions/ -type f); do
         # Assumption is that assertion produces error messages in case they fail
         $A 2> >(sed 's/^/    /') | cat >> test-report
     done
@@ -45,7 +64,7 @@ fi
 
 # If the test leads to a LOST game, check lost game assertions
 if [ "$(cat ${TEST_HOME}/game.result)" == "lost" ]; then
-    for A in $(find ${TEST_HOME}/lost-game-assertions/ -type f); do
+    for A in $(find ${ASSERTIONS_HOME}/lost-game-assertions/ -type f); do
         # Assumption is that assertion produces error messages in case they fail
         $A 2> >(sed 's/^/    /') | cat >> test-report
     done
@@ -59,7 +78,19 @@ if [ -e ${TEST_HOME}/private ]; then
 fi
 
 if [[ -s test-report ]]; then
-    echo "  ${TEST_NAME} FAILED (${TEST_TYPE})" | cat - test-report > temp && mv temp test-report
+    TEST_STATUS="FAILED"
 else
-    echo "  ${TEST_NAME} PASSED (${TEST_TYPE})" | cat - test-report > temp && mv temp test-report
+    TEST_STATUS="PASSED"
+fi
+
+echo "  ${TEST_NAME} ${TEST_STATUS} (${TEST_TYPE})" | cat - test-report > temp && mv temp test-report
+
+cd -
+
+# TODO Clean up should be ensured... RAII?! TRAP the errors?
+# Clean up test execution files if test passed
+if [ "${TEST_STATUS}" == "PASSED" ]; then
+    rm ${TEST_HOME}/output      # Generated
+    rm ${TEST_HOME}/output.diff # Generated
+    rm ${TEST_HOME}/error       # Generated
 fi
